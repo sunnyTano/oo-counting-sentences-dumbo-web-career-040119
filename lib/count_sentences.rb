0e663434525283bb7e6f044fa8b1f6e5d08a0702
Susan Tan <ssusantan9@gmail.com>
2019-04-04 03:45:37 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
-
+  return self.split(/\?|\.|!/).size 
+  # splits the string at every ending punctuation into an array and then gets the size of the array
   end
+  # binding.pry
 end
